cbada2aa3708173d1f2bad76f8d72b7cc95bb9e6
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2021-06-08 16:30:39 +0200

    add changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+ const $menu = document.querySelector('header nav');
+    const $iconeMenu = document.querySelector('.menu');
+
+    $iconeMenu.addEventListener('click', () => {
+        $menu.classList.toggle('active')
+    })
+
+
